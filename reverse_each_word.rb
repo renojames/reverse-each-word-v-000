d19d637600cde7646dfa7e7bd09a22315cd7ebcd
Reno Sorensen <renojames19@gmail.com>
2018-02-19 00:52:36 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(sentance)
-  sentance.split(" ")
+  reversed_sentance
+  sentance = sentance.split(" ")
+  sentance.each do |word|
+    reversed_sentance << word.reverse
+  end
+  reversed_sentance
 end
