4a6e7545ca07304dfea31579af45a8db885e7484
Reno Sorensen <renojames19@gmail.com>
2018-02-19 01:02:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,8 +11,7 @@
 def reverse_each_word(sentance)
   reversed_sentance = []
   sentance = sentance.split(" ")
-  sentance.each do |word|
-    reversed_sentance << word.reverse
+  sentance.collect do |word|
+    word.reverse
   end
-  reversed_sentance.join(" ")
 end
